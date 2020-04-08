a433d23710e09ceb307c160b0c8961b9ed59435c
James Halsall <james.halsall@inviqa.com>
2020-04-08 15:22:44 +0100

    Iterate

--- simple.py
+++ simple.py
@@ -1,7 +1,7 @@
 from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams.onprem.container import Docker
+from diagrams.onprem.database import Postgresql
+from diagrams.onprem.network import Traefik
 
 with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+    Traefik("lb") >> Docker("web") >> Postgresql("userdb")
